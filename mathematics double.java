a2cd2aa1e9617f21fc30d3fd4e3f37f77c819c64
selvakumarMsk <43032313+selvakumarMsk@users.noreply.github.com>
2018-09-07 12:25:02 +0530

    Add files via upload

---
+++ mathematics double.java
@@ -0,0 +1,17 @@
+ class Main {
+  public static void main(String[] args) {
+    System.out.println("add sub mux and div 8,2");
+    double a,c,d,e,f;
+    int b;
+    a=5.205;
+    b=34;
+    c=a+b;
+    d=a-b;
+    e=a*b;
+    f=a/b;
+    System.out.println("add="+c);
+    System.out.println("sub="+d);
+    System.out.println("mux="+e);
+    System.out.println("div="+f);
+  }
+}
