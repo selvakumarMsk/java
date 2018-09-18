fdacc1ada3093e1960cfd68c88544c6445e58557
selvakumarMsk <43032313+selvakumarMsk@users.noreply.github.com>
2018-09-18 14:39:23 +0530

    Add files via upload

---
+++ details.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.util.*;
+class Main{
+  public static void main (String[] args)
+  {
+    Scanner sc=new Scanner(System.in);
+    String name,city,marriage,sex;
+    int age,height;
+    double city population;
+    System.out.println("enter the name");
+   name=sc.next();
+    System.out.println("enter the city");
+   city=sc.next();
+    System.out.println("enter the age");
+    age=sc.nextInt();
+    System.out.println("enter the marrige");
+    marriage=sc.next();
+    System.out.println("enter the height");
+   height=sc.nextInt();
+    System.out.println("enter the sex");
+    sex=sc.next();
+    System.out.println("enter the citypopulation");
+    citypopulation=sc.nextInt();
+    name=name;
+    age=age;
+    city=city;
+    marriage=marriage;
+    height=height;
+    citypopulation=citypopulation;
+    System.out.println("name=:"+name);
+    System.out.println("age=:"+age);
+    System.out.println("city=:"+city);
+    System.out.println("marrige=:"+marriage);
+    System.out.println("citypopulation=:"+citypopulation);
+            
+  }
+}
