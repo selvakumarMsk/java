7709e47c9c1b3a94b00f98f1cd894f530e452fb4
selvakumarMsk <43032313+selvakumarMsk@users.noreply.github.com>
2018-10-03 12:12:08 +0530

    Add files via upload

---
+++ checknumber.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class checknumberposiveornegative{
+  public static void main(String[]arg){
+    Scanner ka=new Scanner(System.in);
+    int a;
+    System.out.println("enter the number");
+    a=ka.nextInt();
+    if(a==0)
+     {
+      System.out.println("zero");
+    }
+    else if(a>0)
+    {
+      System.out.println("positive");
+    }
+    else if(a<0)
+    {
+      System.out.println("negative");
+    }
+    else 
+    {
+      System.out.println("invalid");
+    }
+  }
+}
