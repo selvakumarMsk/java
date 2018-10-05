4b89c9a0e365518f5d42624b0c961422c240f19e
selvakumarMsk <43032313+selvakumarMsk@users.noreply.github.com>
2018-10-05 12:22:01 +0530

    Add files via upload

---
+++ leap year.java
@@ -0,0 +1,35 @@
+
+import java.util.Scanner;
+class Main
+{
+   public static void main(String args[])
+    {
+        Scanner s = new Scanner(System.in);
+        int year = s.nextInt();
+        boolean flag = false;
+        if(year % 400 == 0)
+        {
+            flag = true;
+        }
+        else if (year % 100 == 0)
+        {
+            flag = false;
+        }
+        else if(year % 4 == 0)
+        {
+            flag = true;
+        }
+        else
+        {
+            flag = false;
+        }
+        if(flag)
+        {
+            System.out.println(" yes");
+        }
+        else
+        {
+            System.out.println("no");
+        }
+    }
+}
